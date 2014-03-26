b250c4fe21cc90370df1a85a86e8e1e0fc420198
colinwu <ruiwu1990324@gmail.com>
2014-03-26 09:12:32 -0500

    40 problems solved

---
+++ src/InterleavingString.java
@@ -0,0 +1,42 @@
+
+public class InterleavingString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="a";
+		String s2="b";
+		String s3 = "aba";
+		System.out.print(isInterleave(s1, s2, s3));
+	}
+	public static boolean isInterleave(String s1, String s2, String s3) {
+		if(s3.length()!=s1.length()+s2.length())
+			return false;
+		if(s1.equals(""))
+			return s2.equals(s3);
+		if(s2.equals(""))
+			return s1.equals(s3);
+		char[] c1=s1.toCharArray();
+		char[] c2=s2.toCharArray();
+		boolean[][] matrix=new boolean[s1.length()+1][s2.length()+1];
+		matrix[0][0]=true;
+		for (int i = 1; i < s2.length()+1; i++) {
+			if(matrix[0][i-1] && c2[i-1]==s3.charAt(i-1))
+				matrix[0][i]=true;
+		}
+		for (int i = 1; i < s1.length()+1; i++) {
+			if(matrix[i-1][0] && c1[i-1]==s3.charAt(i-1))
+				matrix[i][0]=true;
+		}
+		for (int i = 1; i < s1.length()+1; i++) {
+			for (int j = 1; j <s2.length()+1; j++) {
+				//go right, take a char from c2
+				if(matrix[i][j-1] && s3.charAt(i+j-1)==c2[j-1])
+					matrix[i][j]=true;
+				//go down, take a char from c1
+				if(matrix[i-1][j] && s3.charAt(i+j-1)==c1[i-1])
+					matrix[i][j]=true;
+			}
+		}
+		return matrix[s1.length()][s2.length()];
+    }
+}
---
+++ src/WordSearch.java
@@ -0,0 +1,68 @@
+
+public class WordSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[][] board={{'A','B','C','E'},{'S','F','E','S'},{'A','D','E','E'}};
+		System.out.println(exist(board, "ABCESEEEFS"));
+	}
+
+	public static boolean exist(char[][] board, String word) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (board[i][j] == word.charAt(0)) {
+					boolean[][] searched = new boolean[board.length][board[0].length];
+					if (existHelper(board, word.substring(1), searched.clone(),
+							i, j))
+						return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean existHelper(char[][] board, String word,
+			boolean[][] searched, int x, int y) {
+		if (word.equals(""))
+			return true;
+		char c = word.charAt(0);
+		searched[x][y] = true;
+		// go left
+		if (y - 1 >= 0 && !searched[x][y - 1] && board[x][y - 1] == c) {
+			boolean[][] copyS = arrayCopy(searched);
+			if (existHelper(board, word.substring(1), copyS, x, y - 1))
+				return true;
+		}
+		// go right
+		if (y + 1 < board[0].length && !searched[x][y + 1]
+				&& board[x][y + 1] == c) {
+			boolean[][] copyS = arrayCopy(searched);
+			if (existHelper(board, word.substring(1), copyS, x, y + 1))
+				return true;
+		}
+		// go up
+		if (x - 1 >= 0 && !searched[x - 1][y] && board[x - 1][y] == c) {
+			boolean[][] copyS = arrayCopy(searched);
+			if (existHelper(board, word.substring(1), copyS, x - 1, y))
+				return true;
+		}
+		// go down
+		if (x + 1 < board.length && !searched[x + 1][y] && board[x + 1][y] == c) {
+			boolean[][] copyS = arrayCopy(searched);
+			if (existHelper(board, word.substring(1), copyS, x + 1, y))
+				return true;
+		}
+		return false;
+	}
+
+	private static boolean[][] arrayCopy(boolean[][] searched) {
+		// TODO Auto-generated method stub
+		boolean[][] copyS = new boolean[searched.length][searched[0].length];
+		for (int i = 0; i < copyS.length; i++) {
+			for (int j = 0; j < copyS[0].length; j++) {
+				copyS[i][j] = searched[i][j];
+			}
+		}
+		return copyS;
+	}
+}
