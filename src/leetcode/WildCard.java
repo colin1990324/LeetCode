d86ef9ac147f618a080dd8c8075ec370fad2182c
colinwu <ruiwu1990324@gmail.com>
2014-04-21 15:04:25 -0500

    update

--- src/leetcode/WildCard.java
+++ src/leetcode/WildCard.java
@@ -11,7 +11,10 @@ public class WildCard {
 
 	public static void main(String[] args) {
 		String string = "1?00?101";
-		ArrayList<String> outoutArrayList = expand(string);
+		String string2 = "1?";
+		String string3 = "1";
+		String string4 = "?1";
+		ArrayList<String> outoutArrayList = expand(string4);
 		for (String s : outoutArrayList) {
 			System.out.println(s);
 		}
@@ -20,12 +23,15 @@ public class WildCard {
 	public static ArrayList<String> expand(String s) {
 		ArrayList<String> output = new ArrayList<String>();
 		String[] splits = s.split("\\?");
+		//empty String
 		if(s.length()==0)
 			return output;
-		if(splits.length==1){
+		//no '?'
+		if(splits.length==1 && !s.substring(s.length()-1).equals("\\?")){
 			output.add(s);
 			return output;
 		}
+		//
 		int length = splits[0].length() + 1;
 		helper(s.substring(length, s.length()), splits[0] + "1", output);
 		helper(s.substring(length, s.length()), splits[0] + "0", output);
