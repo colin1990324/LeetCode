002a3b38cbbb6108d8e8be9504bdf122fa295a4d
colinwu <ruiwu1990324@gmail.com>
2014-04-23 21:46:28 -0500

    update

---
+++ src/leetcode/DecodeWays.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * http://oj.leetcode.com/problems/decode-ways/
+ * @author ColinMac
+ */
+
+public class DecodeWays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/leetcode/Reorder.java
+++ src/leetcode/Reorder.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-public class MyString {
+public class Reorder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
