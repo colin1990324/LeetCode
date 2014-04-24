b3157b9b24e774a9e0ac6e732e8f7dfc9f03424e
colinwu <ruiwu1990324@gmail.com>
2014-04-23 23:38:42 -0500

    update

--- src/leetcode/DecodeWays.java
+++ src/leetcode/DecodeWays.java
@@ -2,6 +2,7 @@ package leetcode;
 
 /**
  * http://oj.leetcode.com/problems/decode-ways/
+ * 
  * @author ColinMac
  */
 
@@ -9,7 +10,31 @@ public class DecodeWays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String inString = "101";
+		System.out.println(numDecodings(inString));
 	}
 
+	public static int numDecodings(String s) {
+		int length = s.length();
+		if (length == 0 || s.charAt(0) == '0')
+			return 0;
+		int[] vector = new int[length + 1];
+		vector[0] = 1;
+		vector[1] = 1;
+		for (int i = 2; i < vector.length; i++) {
+			int v = Integer.valueOf(s.substring(i - 2, i));
+			if (v <= 26) {
+				if (v > 9)
+					vector[i] += vector[i - 2];
+				if (s.charAt(i - 1) != '0')
+					vector[i] += vector[i - 1];
+			} else {
+				if (s.charAt(i - 1) != '0')
+					vector[i] += vector[i - 1];
+				else if (i != length)
+					return 0;
+			}
+		}
+		return vector[length];
+	}
 }
---
+++ src/leetcode/ReorderString.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+public class ReorderString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string = "007799aabbccddeeff113355zz";
+		String string2 = "abcdefabcdefabcdefaaaaaaaaaaaaaabbbbbbbddddddee";
+		String string3 = "1234.89898";
+		System.out.println(reorder(string3));
+	}
+
+	// http://www.coderblog.cn/View/18/%E5%BE%AE%E8%BD%AF%E7%AC%94%E8%AF%95%E9%A2%98%EF%BC%9AString%20reorder/
+	public static String reorder(String in) {
+		// 0-25: a-z
+		// 26-35:0-9
+		int[] counter = new int[36];
+		int sum = 0;
+		char[] chars = in.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			char c = chars[i];
+			if (47 < c && c < 58)
+				counter[c - 22]++;
+			else if (96 < c && c < 123)
+				counter[c - 97]++;
+			else
+				return "<invalid input string>";
+			sum++;
+		}
+		String output = "";
+		while (sum > 0) {
+			for (int i = 26; i < 36; i++) {
+				if (counter[i] > 0) {
+					output += (char) (i + 22);
+					counter[i]--;
+					sum--;
+				}
+			}
+			for (int i = 0; i < 26; i++) {
+				if (counter[i] > 0) {
+					output += (char) (i + 97);
+					counter[i]--;
+					sum--;
+				}
+			}
+		}
+		return output;
+	}
+}
